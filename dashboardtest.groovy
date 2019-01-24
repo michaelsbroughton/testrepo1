c5682c159b1ba57bee9f555f8e3a0c8c36672b4b
Michael Broughton <michaelsbroughton@yahoo.com>
2019-01-24 14:28:53 -0500

    Update dashboardtest.groovy

--- dashboardtest.groovy
+++ dashboardtest.groovy
@@ -1,15 +1,15 @@
 pipeline {
+	agent any
 	parameters {
 	    choice (name: 'Stack', choices: ['dev', 'test', 'prod'], description: 'Stack to deploy')
 	    choice (name: 'Customer', choices: ['Akron', 'Houston Methodist', 'Yale'], description: 'Customer environment to deploy')
 	    choice (name: 'Product', choices: ['PeraServer', 'PeraTrend', 'PeraWatch'], description: 'Stack to deploy')
 	    string (name: 'Version', defaultValue: 'Enter version X.X.X.X (i.e. 4.2.0.15)', description: 'Version including build to deploy', trim: true)
 	    }
-	agent any
 	stages {
 	    stage('Deploy') {
 		    steps {
-			    echo 'Deploying ' + ''${params.Product}'' + 'v' + ''${params.Version}'' + ' to ' + ''${params.Customer}'' + ' ' + ''${params.Stack}'' + ' stack'
+			    echo "Deploying ${params.Product} v${params.Version} to ${params.Customer} ${params.Stack} stack"
 		    }
 	    }
 	}
